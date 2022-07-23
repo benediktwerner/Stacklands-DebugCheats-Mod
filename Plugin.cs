433df358be66eca6eb49c8fc006e62ffefe78c69
Benedikt Werner <1benediktwerner@gmail.com>
2022-07-23 11:12:02 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace DebugDeleteCard
+{
+    [BepInPlugin("de.benediktwerner.stacklands.debugdeletecard", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPatch(typeof(WorldManager), "DebugUpdate")]
+        [HarmonyPrefix]
+        private static void WorldManager__DebugUpdate__Prefix(ref WorldManager __instance)
+        {
+            if (InputController.instance.GetKeyDown(Key.Delete) && __instance.HoveredCard != null)
+            {
+                Villager villager = __instance.HoveredCard.CardData as Villager;
+                if (villager != null)
+                {
+                    villager.Damage(100);
+                    return;
+                }
+                __instance.HoveredCard.DestroyCard(false, true);
+            }
+        }
+    }
+}
