48bc21c97022e2075b174d96ab0d0404a4c4209d
Benedikt Werner <1benediktwerner@gmail.com>
2022-07-24 16:49:52 +0200

    Add more debug cheats

--- Plugin.cs
+++ Plugin.cs
@@ -1,31 +1,79 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine.InputSystem;
 
-namespace DebugDeleteCard
+namespace DebugCheats
 {
-    [BepInPlugin("de.benediktwerner.stacklands.debugdeletecard", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInPlugin("de.benediktwerner.stacklands.debugcheats", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        public static ConfigEntry<bool> Enabled;
+        public static ConfigEntry<bool> DebugKeysEnabled;
+        public static ConfigEntry<bool> DisableFood;
+        public static ConfigEntry<bool> OverrideMonthLength;
+        public static ConfigEntry<float> MonthLength;
+
         private void Awake()
         {
+            Enabled = Config.Bind("General", "Enabled", true, "Can be used to disable the whole mod");
+            DebugKeysEnabled = Config.Bind("General", "Debug Keys Enabled", true, "Enable additional debugging shortcuts");
+            DisableFood = Config.Bind("General", "Disable Food", true, "Disable villager food requirements");
+            OverrideMonthLength = Config.Bind("General", "Override Month Length", true, "Override month length with the value set in 'Month Length' below.");
+            MonthLength = Config.Bind("General", "Month Length", float.PositiveInfinity, "How long months should be. Vanilla is 90 for Short, 120 for Normal, 200 for Long.");
+
             Harmony.CreateAndPatchAll(typeof(Plugin));
         }
 
         [HarmonyPatch(typeof(WorldManager), "DebugUpdate")]
         [HarmonyPrefix]
-        private static void WorldManager__DebugUpdate__Prefix(ref WorldManager __instance)
+        private static void WorldManager__DebugUpdate(ref WorldManager __instance)
         {
-            if (InputController.instance.GetKeyDown(Key.Delete) && __instance.HoveredCard != null)
+            if (!DebugKeysEnabled.Value) return;
+
+            var card = __instance.HoveredCard;
+            if ((InputController.instance.GetKeyDown(Key.Delete) || (InputController.instance.GetKey(Key.Delete) && InputController.instance.GetKey(Key.RightShift))) && card != null)
             {
-                Villager villager = __instance.HoveredCard.CardData as Villager;
-                if (villager != null)
-                {
-                    villager.Damage(100);
-                    return;
-                }
-                __instance.HoveredCard.DestroyCard(false, true);
+                __instance.DestroyStack(card);
             }
+
+            if (InputController.instance.GetKeyDown(Key.F1))
+            {
+                GameScreen.instance.DebugScreen.gameObject.SetActive(!GameScreen.instance.DebugScreen.gameObject.activeInHierarchy);
+            }
+
+            if (GameCanvas.instance.AboveMeOrMyChildren(GameScreen.instance.DebugScreen.rectTransform, InputController.instance.ClampedMousePosition()) && InputController.instance.InputString.Length > 0)
+            {
+                GameScreen.instance.JumpToDebugPosition(InputController.instance.InputString[0]);
+            }
+
+            if (InputController.instance.GetKeyDown(Key.F2))
+            {
+                __instance.CreateGold(__instance.MiddleOfBoard(), 5, true);
+            }
+
+            if (InputController.instance.GetKeyDown(Key.C) && card != null)
+            {
+                __instance.CreateCard(card.transform.position, card.CardData.Id);
+            }
+        }
+
+        [HarmonyPatch(typeof(WorldManager), "GetRequiredFoodCount")]
+        [HarmonyPrefix]
+        private static void NoFood(ref bool __runOriginal, ref int __result)
+        {
+            if (!DisableFood.Value) return;
+            __runOriginal = false;
+            __result = 0;
+        }
+
+        [HarmonyPatch(typeof(WorldManager), "MonthTime", MethodType.Getter)]
+        [HarmonyPrefix]
+        private static void InfiniteMonths(ref bool __runOriginal, ref float __result)
+        {
+            if (!OverrideMonthLength.Value) return;
+            __runOriginal = false;
+            __result = MonthLength.Value;
         }
     }
 }
---
+++ Readme.md
@@ -0,0 +1,32 @@
+# Stacklands DebugCheats Mod
+
+Adds various (configurable) debug cheats to make testing stuff easier:
+
+- Month never ends (can also be changed to a custom length instead)
+- Villagers don't need food anymore
+- Press F1 to open the card spawn menu (instead of K+O+F1 in vanilla)
+- When hovering over the card spawn menu, press a letter to jump to cards starting with the name
+- Press F2 to spawn 5 coins
+- Press C while hovering over a card to duplicate it
+- Press Delete to delete the card below the cursor and its children
+- Hold Shift+Delete to continuously delete cards the cursor touches
+
+To configure or disable the cheats, edit the `BepInEx/config/de.benediktwerner.stacklands.debugcheats.cfg` file which appears after starting the game once.
+
+See also the HigherSidebar mod to increase the height of the card spawn menu.
+
+## Manual Installation
+This mod requires BepInEx to work. BepInEx is a modding framework which allows multiple mods to be loaded.
+
+1. Download and install BepInEx from the [Thunderstore](https://stacklands.thunderstore.io/package/BepInEx/BepInExPack_Stacklands/).
+4. Download this mod and extract it into `BepInEx/plugins/`
+5. Launch the game
+
+## Links
+- Github: https://github.com/benediktwerner/Stacklands-DebugCheats-Mod
+- Thunderstore: https://stacklands.thunderstore.io/package/benediktwerner/DebugCheats
+- Nexusmods: https://www.nexusmods.com/stacklands/mods/8
+
+## Changelog
+
+- v1.0: Initial release
