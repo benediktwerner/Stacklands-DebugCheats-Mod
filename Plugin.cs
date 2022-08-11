b33b8de10754a395ccfe792baea73d90083562ff
Benedikt Werner <1benediktwerner@gmail.com>
2022-08-11 13:47:02 +0200

    Format

--- Plugin.cs
+++ Plugin.cs
@@ -20,17 +20,48 @@ namespace DebugCheats
         private void Awake()
         {
             Enabled = Config.Bind("General", "Enabled", true, "Can be used to disable the whole mod");
-            DebugKeysEnabled = Config.Bind("General", "Debug Keys Enabled", true, "Enable additional debugging shortcuts");
-            DisableStarving = Config.Bind("General", "Disable Starving", false, "Villagers will still eat if there is food but they won't die if there is none.");
-            DisableEating = Config.Bind("General", "Disable Eating", true, "Villagers won't need food and won't eat any even if there is some.");
-            OverrideMonthLength = Config.Bind("General", "Override Month Length", true, "Override month length with the value set in 'Month Length' below.");
-            OverrideMaxCards = Config.Bind("General", "Override Max Cards", 500, "Override maximum number of cards. Set to -1 to use the game's default calculation.");
-            MonthLength = Config.Bind("General", "Month Length", float.PositiveInfinity, "How long months should be. Vanilla is 90 for Short, 120 for Normal, 200 for Long.");
+            DebugKeysEnabled = Config.Bind(
+                "General",
+                "Debug Keys Enabled",
+                true,
+                "Enable additional debugging shortcuts"
+            );
+            DisableStarving = Config.Bind(
+                "General",
+                "Disable Starving",
+                false,
+                "Villagers will still eat if there is food but they won't die if there is none."
+            );
+            DisableEating = Config.Bind(
+                "General",
+                "Disable Eating",
+                true,
+                "Villagers won't need food and won't eat any even if there is some."
+            );
+            OverrideMonthLength = Config.Bind(
+                "General",
+                "Override Month Length",
+                true,
+                "Override month length with the value set in 'Month Length' below."
+            );
+            OverrideMaxCards = Config.Bind(
+                "General",
+                "Override Max Cards",
+                500,
+                "Override maximum number of cards. Set to -1 to use the game's default calculation."
+            );
+            MonthLength = Config.Bind(
+                "General",
+                "Month Length",
+                float.PositiveInfinity,
+                "How long months should be. Vanilla is 90 for Short, 120 for Normal, 200 for Long."
+            );
 
             Harmony.CreateAndPatchAll(typeof(Plugin));
         }
 
-        private static bool HoldingShift() {
+        private static bool HoldingShift()
+        {
             return InputController.instance.GetKey(Key.LeftShift) || InputController.instance.GetKey(Key.RightShift);
         }
 
@@ -38,24 +69,40 @@ namespace DebugCheats
         [HarmonyPrefix]
         private static void WorldManager__Update(ref WorldManager __instance)
         {
-            if (__instance.MonthTimer > __instance.MonthTime * 1.5) {
+            if (__instance.MonthTimer > __instance.MonthTime * 1.5)
+            {
                 __instance.MonthTimer = __instance.MonthTime / 2;
             }
 
-            if (!Enabled.Value || !DebugKeysEnabled.Value) return;
+            if (!Enabled.Value || !DebugKeysEnabled.Value)
+                return;
 
             var card = __instance.HoveredCard;
-            if ((InputController.instance.GetKeyDown(Key.Delete) || (InputController.instance.GetKey(Key.Delete) && HoldingShift())) && card != null)
+            if (
+                (
+                    InputController.instance.GetKeyDown(Key.Delete)
+                    || (InputController.instance.GetKey(Key.Delete) && HoldingShift())
+                )
+                && card != null
+            )
             {
                 __instance.DestroyStack(card);
             }
 
             if (InputController.instance.GetKeyDown(Key.F1))
             {
-                GameScreen.instance.DebugScreen.gameObject.SetActive(!GameScreen.instance.DebugScreen.gameObject.activeInHierarchy);
+                GameScreen.instance.DebugScreen.gameObject.SetActive(
+                    !GameScreen.instance.DebugScreen.gameObject.activeInHierarchy
+                );
             }
 
-            if (GameCanvas.instance.AboveMeOrMyChildren(GameScreen.instance.DebugScreen.rectTransform, InputController.instance.ClampedMousePosition()) && InputController.instance.InputString.Length > 0)
+            if (
+                GameCanvas.instance.AboveMeOrMyChildren(
+                    GameScreen.instance.DebugScreen.rectTransform,
+                    InputController.instance.ClampedMousePosition()
+                )
+                && InputController.instance.InputString.Length > 0
+            )
             {
                 GameScreen.instance.JumpToDebugPosition(InputController.instance.InputString[0]);
             }
@@ -68,10 +115,12 @@ namespace DebugCheats
 
             if (InputController.instance.GetKeyDown(Key.C) && card != null)
             {
-                if (HoldingShift()) {
+                if (HoldingShift())
+                {
                     var pos = card.transform.position;
                     var parent = __instance.CreateCard(pos, card.CardData.Id).MyGameCard;
-                    while (card.Child != null) {
+                    while (card.Child != null)
+                    {
                         var child = __instance.CreateCard(pos, card.Child.CardData.Id).MyGameCard;
                         parent.Child = child;
                         child.Parent = parent;
@@ -79,7 +128,8 @@ namespace DebugCheats
                         card = card.Child;
                     }
                 }
-                else {
+                else
+                {
                     __instance.CreateCard(card.transform.position, card.CardData.Id);
                 }
             }
@@ -89,7 +139,8 @@ namespace DebugCheats
         [HarmonyPrefix]
         private static void NoFood(ref bool __runOriginal, ref int __result)
         {
-            if (!Enabled.Value || !DisableStarving.Value) return;
+            if (!Enabled.Value || !DisableStarving.Value)
+                return;
             __runOriginal = false;
             __result = 0;
         }
@@ -98,7 +149,8 @@ namespace DebugCheats
         [HarmonyPrefix]
         private static void NoEating(ref bool __runOriginal, ref int __result)
         {
-            if (!Enabled.Value || !DisableEating.Value) return;
+            if (!Enabled.Value || !DisableEating.Value)
+                return;
             __runOriginal = false;
             __result = 0;
         }
@@ -106,10 +158,11 @@ namespace DebugCheats
         [HarmonyPrefix]
         [HarmonyPatch(typeof(WorldManager), "GetMaxCardCount")]
         [HarmonyPatch(new Type[0])]
-        [HarmonyPatch(new Type[]{ typeof(GameBoard) })]
+        [HarmonyPatch(new Type[] { typeof(GameBoard) })]
         private static void MaxCards(ref bool __runOriginal, ref int __result)
         {
-            if (!Enabled.Value || OverrideMaxCards.Value < 0) return;
+            if (!Enabled.Value || OverrideMaxCards.Value < 0)
+                return;
             __runOriginal = false;
             __result = OverrideMaxCards.Value;
         }
@@ -118,7 +171,8 @@ namespace DebugCheats
         [HarmonyPrefix]
         private static void InfiniteMonths(ref bool __runOriginal, ref float __result)
         {
-            if (!Enabled.Value || !OverrideMonthLength.Value) return;
+            if (!Enabled.Value || !OverrideMonthLength.Value)
+                return;
             __runOriginal = false;
             __result = MonthLength.Value;
         }
